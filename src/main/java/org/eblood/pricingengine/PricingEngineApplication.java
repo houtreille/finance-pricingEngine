bfe870874445e382e67c575d866ada17dd03f43d
mhoutreille <mhoutreille@pictet-technologies.com>
2022-10-31 14:49:58 +0100

    first commit

--- src/main/java/org/eblood/pricingengine/PricingEngineApplication.java
+++ src/main/java/org/eblood/pricingengine/PricingEngineApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PricingEngineApplication {
 
     public static void main(String[] args ) {
-        SpringApplication.run(PricingEngineApplication.class, args );
+        SpringApplication.run(PricingEngineApplication.class, args  );
     }
 
 }
