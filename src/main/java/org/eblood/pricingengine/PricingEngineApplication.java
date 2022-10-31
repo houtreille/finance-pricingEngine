4c6b5e7fdf76c6442b6856318e89edd876ecf8db
mhoutreille <mhoutreille@pictet-technologies.com>
2022-10-31 14:46:22 +0100

    first Commit on pricing ENgine

---
+++ src/main/java/org/eblood/pricingengine/PricingEngineApplication.java
@@ -0,0 +1,13 @@
+package org.eblood.pricingengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricingEngineApplication {
+
+    public static void main(String[] args ) {
+        SpringApplication.run(PricingEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/eblood/pricingengine/PricingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eblood.pricingengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
